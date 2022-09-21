db68c841cf1a7009398cef11b350dd0452c64809
Shuvo-Jyoti-Sarkar <shuvojyoti98@gmail.com>
2022-09-21 22:12:05 +0600

    y first node server is ready

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from my personal smarty pant with auto restart')
+});
+
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: '017888888' },
+    { id: 2, name: 'Sabnoor', email: 'sabnoor@gmail.com', phone: '017888888' },
+    { id: 3, name: 'Suchorita', email: 'suchorita@gmail.com', phone: '017888888' },
+    { id: 4, name: 'Suchonda', email: 'suchonda@gmail.com', phone: '017888888' },
+    { id: 5, name: 'Srabonti', email: 'srabonti@gmail.com', phone: '017888888' },
+    { id: 6, name: 'Sabila', email: 'sabila@gmail.com', phone: '017888888' },
+    { id: 7, name: 'Sohana', email: 'sohana@gmail.com', phone: '017888888' }
+]
+
+app.get('/users', (req, res) => {
+    // filter by search query parameter
+    if (req.query.name) {
+        const search = req.query.name;
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user);
+});
+
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'apple', 'oranges']);
+});
+
+app.get('/fruits/mango/fazzle', (req, res) => {
+    res.send('sour sour fazle flavour');
+});
+
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
